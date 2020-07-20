223fad41a54ef7f37a63d2403425c715c91e4782
Diana Soponar <32791081+dianasoponar@users.noreply.github.com>
2020-07-20 15:58:57 +0200

    Update apiKeys.js

--- src/components/apiKeys.js
+++ src/components/apiKeys.js
@@ -1,3 +1,3 @@
-export const GeocodeApiKey = "AIzaSyBESc_iTGS2Qys0bHcjeChJk9WChsHp4bI";
-export const WeatherApiKey = "4641ad3e90202b3e5b93bb82490ca04f";
-export const FirebaseApiKey = "AIzaSyCuAsq3p_tyEKDTmRjIXC_MmwQthduKyw4";
+export const GeocodeApiKey = "";
+export const WeatherApiKey = "";
+export const FirebaseApiKey = "";
